cf25c7898deecab8b3b8ac44f9f219bc8125a499
jizero1 <156683069+jizero1@users.noreply.github.com>
2024-04-09 00:16:35 +0900

    Title: 변수 값 교체하기 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240409/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        int b = 5;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.printf("%d",a);
+        System.out.print("\n");
+        System.out.printf("%d",b);
+    }
+}
