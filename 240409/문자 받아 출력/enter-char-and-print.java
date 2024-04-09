a80e244630752ce9f8bbde52410507b5a00a4819
jizero1 <156683069+jizero1@users.noreply.github.com>
2024-04-09 18:34:48 +0900

    Title: 문자 받아 출력, Time: 110ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        char c = a.charAt(0);
+        System.out.print(c);
+    }
+}
