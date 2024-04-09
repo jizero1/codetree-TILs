f425f04a198ae4b50a26f2908fc02f6ece1db784
jizero1 <156683069+jizero1@users.noreply.github.com>
2024-04-09 23:04:31 +0900

    Title: 전화번호 바꾸기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner s = new Scanner(System.in);
+        s.useDelimiter("-");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        System.out.print("0"+a+"-"+c+"-"+b);
+    }
+}
