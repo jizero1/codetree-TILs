7f57271169dbb569c99e833fb1c4f3b5277e8c3f
jizero1 <156683069+jizero1@users.noreply.github.com>
2024-04-09 18:26:47 +0900

    Title: 실수 입력받아 계산, Time: 169ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner s = new Scanner(System.in);
+        float a = s.nextFloat();
+        float b = s.nextFloat();
+        System.out.printf("%.2f",a+b);
+    }
+}
